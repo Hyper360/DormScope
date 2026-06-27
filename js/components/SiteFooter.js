0664dea6e4ce0debe568a83987106c7e7b4d81ef
Hyper360 <redcliffhero0007@gmail.com>
2026-06-26 21:12:39 -0400

    Add base styling for navbar and footer

--- js/components/SiteFooter.js
+++ js/components/SiteFooter.js
@@ -7,7 +7,7 @@ class SiteFooter extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
     <footer>
-      <p>DormScope 2026</p>
+      <p id="footer-title">DormScope 2026</p>
       <p><a href="index.html">Home</a></p>
       <p><a href="search.html">Search</a></p>
       <p><a href="about.html">About Us</a></p>
